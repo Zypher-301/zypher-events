fcfb678954371dc6fc9d9d4be944bd608a190953
yyzTom <162637160+yyzTom@users.noreply.github.com>
2025-10-29 23:54:24 -0600

    Added Dependency For SwipeRefreshLayout

--- code/app/build.gradle.kts
+++ code/app/build.gradle.kts
@@ -47,6 +47,7 @@ dependencies {
     implementation(libs.navigation.fragment)
     implementation(libs.navigation.ui)
     implementation(libs.firebase.firestore)
+    implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)

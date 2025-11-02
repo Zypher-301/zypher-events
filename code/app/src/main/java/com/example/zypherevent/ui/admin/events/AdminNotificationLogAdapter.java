8c07b47bc0819a772cd467fff2419c941c7d1a36
ADS2001 <arunavodutta21@gmail.com>
2025-11-02 13:42:24 -0700

    Completed Admin Stories: US 03.08.01 (and probably US 03.07.01 as well)

--- code/app/src/main/java/com/example/zypherevent/ui/admin/events/AdminNotificationLogAdapter.java
+++ code/app/src/main/java/com/example/zypherevent/ui/admin/events/AdminNotificationLogAdapter.java
@@ -6,24 +6,38 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+import com.example.zypherevent.Notification; // Use the REAL Notification model
 import com.example.zypherevent.R;
-import com.example.zypherevent.model.AdminNotificationLog;
 import java.util.List;
+
 /**
  * @author Arunavo Dutta
- * @version 1.0
- * @see AdminNotificationLog
+ * @version 2.0
+ * @see Notification
  * @see res/layout/fragment_admin_item_notification_log.xml
  */
-
 public class AdminNotificationLogAdapter extends RecyclerView.Adapter<AdminNotificationLogAdapter.LogViewHolder> {
 
-    private List<AdminNotificationLog> logList;
+    // Use the real Notification model
+    private List<Notification> logList;
 
-    public AdminNotificationLogAdapter(List<AdminNotificationLog> logList) {
+    public AdminNotificationLogAdapter(List<Notification> logList) {
         this.logList = logList;
     }
 
+    /**
+     * Called when RecyclerView needs a new {@link LogViewHolder} of the given type to represent
+     * an item.
+     * <p>
+     * This new ViewHolder should be constructed with a new View that can represent the items
+     * of the given type. You can either create a new View manually or inflate it from an XML
+     * layout file.
+     *
+     * @param parent   The ViewGroup into which the new View will be added after it is bound to
+     *                 an adapter position.
+     * @param viewType The view type of the new View.
+     * @return A new LogViewHolder that holds a View of the given view type.
+     */
     @NonNull
     @Override
     public LogViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -32,21 +46,57 @@ public class AdminNotificationLogAdapter extends RecyclerView.Adapter<AdminNotif
         return new LogViewHolder(view);
     }
 
+    /**
+     * Binds the data from the {@link Notification} object at a specific position in the list
+     * to the views within the {@link LogViewHolder}.
+     * <p>
+     * This method is called by the RecyclerView to display the data at the specified position.
+     * It retrieves the Notification object and populates the TextViews in the ViewHolder with
+     * the notification's header, body, sender ID, and unique ID. The 'group' TextView is hidden
+     * as it is not used.
+     *
+     * @param holder   The ViewHolder which should be updated to represent the contents of the
+     *                 item at the given position in the data set.
+     * @param position The position of the item within the adapter's data set.
+     */
     @Override
     public void onBindViewHolder(@NonNull LogViewHolder holder, int position) {
-        AdminNotificationLog log = logList.get(position);
-        holder.message.setText(log.getMessage());
-        holder.event.setText("Event: " + log.getEvent());
-        holder.group.setText("Group: " + log.getGroup());
-        holder.timestamp.setText(log.getTimestamp());
-        holder.sender.setText(log.getSender());
+        // Get the real Notification object
+        Notification log = logList.get(position);
+
+        // --- Map Notification data to the existing XML layout ---
+
+        // 1. Map notificationBody to notification_message
+        holder.message.setText("\"" + log.getNotificationBody() + "\"");
+
+        // 2. Map sendingUserHardwareID to notification_sender
+        holder.sender.setText("From: " + log.getSendingUserHardwareID());
+
+        // 3. Re-use notification_event to show the header
+        holder.event.setText("Title: " + log.getNotificationHeader());
+
+        // 4. Re-use notification_timestamp to show the ID
+        holder.timestamp.setText("ID: " + log.getUniqueNotificationID().toString());
+
+        // 5. Hide the 'group' field, as we don't have data for it
+        holder.group.setVisibility(View.GONE);
     }
 
+    /**
+     * Returns the total number of items in the data set held by the adapter.
+     *
+     * @return The total number of notifications in the list.
+     */
     @Override
     public int getItemCount() {
         return logList.size();
     }
 
+    /**
+     * ViewHolder class for displaying individual notification log items in the RecyclerView.
+     * It holds and initializes the UI components for a single item, such as TextViews
+     * for the message, event, group, timestamp, and sender.
+     */
     public static class LogViewHolder extends RecyclerView.ViewHolder {
         TextView message, event, group, timestamp, sender;
 

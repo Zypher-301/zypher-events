b1b3ebe46d6658c670fe6d016d433bf1413b224c
britneyKunchidi <kunchidi@ualberta.ca>
2025-11-06 15:41:54 -0700

    US 02.05.02

--- code/app/src/main/java/com/example/zypherevent/ui/organizer/events/OrganizerMyEventsFragment.java
+++ code/app/src/main/java/com/example/zypherevent/ui/organizer/events/OrganizerMyEventsFragment.java
@@ -4,17 +4,18 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
+
 import com.example.zypherevent.R;
 
 /**
  * @author Elliot Chrystal
  *
  * @version 1.0
+ * Basic Organizer "My Events" fragment: inflates fragment_organizer_my_events layout.
  */
 public class OrganizerMyEventsFragment extends Fragment {
 
@@ -22,10 +23,13 @@ public class OrganizerMyEventsFragment extends Fragment {
         // public no-arg constructor required
     }
 
-    @Nullable @Override
+    @Nullable
+    @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
+
+        // Inflate the simple Organizer "My Events" layout
         return inflater.inflate(R.layout.fragment_organizer_my_events, container, false);
     }
 }

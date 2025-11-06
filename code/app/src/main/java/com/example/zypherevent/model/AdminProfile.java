e5802beb5253bdd982792ddd70ed0e5905719641
ADS2001 <arunavodutta21@gmail.com>
2025-11-06 09:01:02 -0700

    Update AdminProfile.java

--- code/app/src/main/java/com/example/zypherevent/model/AdminProfile.java
+++ code/app/src/main/java/com/example/zypherevent/model/AdminProfile.java
@@ -45,21 +45,7 @@ public final class AdminProfile { // Made class final
     public String getPhone() { return phone; }
     public String getEmail() { return email; }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        AdminProfile that = (AdminProfile) o;
-        return Objects.equals(name, that.name) &&
-                Objects.equals(role, that.role) &&
-                Objects.equals(phone, that.phone) &&
-                Objects.equals(email, that.email);
-    }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, role, phone, email);
-    }
 
     @Override
     public String toString() {

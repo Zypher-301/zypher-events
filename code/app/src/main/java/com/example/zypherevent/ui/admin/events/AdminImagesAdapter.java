b36e283479e9edd70bae8b05c7bd35dc4c207bcc
ADS2001 <arunavodutta21@gmail.com>
2025-11-11 21:23:23 -0700

    Updated Admin Images

--- code/app/src/main/java/com/example/zypherevent/ui/admin/events/AdminImagesAdapter.java
+++ code/app/src/main/java/com/example/zypherevent/ui/admin/events/AdminImagesAdapter.java
@@ -11,29 +11,59 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.bumptech.glide.Glide;
 import com.example.zypherevent.Event;
 import com.example.zypherevent.R;
+
 import java.util.List;
 
 /**
- * @author Arunavo Dutta
- * @version 1.2
+ * An adapter for displaying a list of {@link Event} objects that have images
+ * for the admin interface.
+ * <p>
+ * This adapter is responsible for creating and binding views for each event's poster,
+ * allowing an administrator to view the image, see which event it belongs to, and
+ * delete the image (by nullifying its posterURL).
+ * <p>
+ * This fulfills parts of:
+ * <ul>
+ * <li><b>US 03.06.01:</b> As an administrator, I want to be able to browse images.</li>
+ * <li><b>US 03.03.01:</b> As an administrator, I want to be able to remove images.</li>
+ * </ul>
+ *
+ * @author Arunavo Dutta (Refactored)
+ * @version 2.0
  * @see Event
+ * @see AdminImagesFragment
  * @see res/layout/fragment_admin_item_image_card.xml
  */
-
 public class AdminImagesAdapter extends RecyclerView.Adapter<AdminImagesAdapter.ImageViewHolder> {
 
     private List<Event> eventList;
     private OnDeleteListener deleteListener;
 
+    /**
+     * Interface for a callback to be invoked when an event's delete image button is clicked.
+     */
     public interface OnDeleteListener {
-        void onDelete(Event event);
+        void onDelete(Event event, int position);
     }
 
+    /**
+     * Constructs a new AdminImagesAdapter.
+     *
+     * @param eventList      The list of {@link Event} objects (with posters) to display.
+     * @param deleteListener The listener that will be invoked when the delete button is clicked.
+     */
     public AdminImagesAdapter(List<Event> eventList, OnDeleteListener deleteListener) {
         this.eventList = eventList;
         this.deleteListener = deleteListener;
     }
 
+    /**
+     * Inflates the layout for an individual image item and returns a new {@link ImageViewHolder} instance.
+     *
+     * @param parent   The ViewGroup into which the new View will be added.
+     * @param viewType The view type of the new View.
+     * @return A new {@link ImageViewHolder} that holds the View for a single image item.
+     */
     @NonNull
     @Override
     public ImageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -42,42 +72,89 @@ public class AdminImagesAdapter extends RecyclerView.Adapter<AdminImagesAdapter.
         return new ImageViewHolder(view);
     }
 
+    /**
+     * Called by RecyclerView to display the data at the specified position.
+     * <p>
+     * This method updates the contents of the {@link ImageViewHolder#itemView} to reflect the
+     * event item at the given position. It binds the event's name and description, and uses
+     * {@link Glide} to load the {@code posterURL} into the {@link ImageView}.
+     * <p>
+     * It also sets an {@link View.OnClickListener} on the delete button, which invokes the
+     * {@link OnDeleteListener#onDelete(Event, int)} callback.
+     *
+     * @param holder   The {@link ImageViewHolder} which should be updated.
+     * @param position The position of the item within the adapter's data set.
+     */
     @Override
     public void onBindViewHolder(@NonNull ImageViewHolder holder, int position) {
         Event event = eventList.get(position);
 
+        // We re-use the text views from the layout to show the event name and description
         holder.eventName.setText(event.getEventName());
         holder.eventDescription.setText(event.getEventDescription());
 
+        // Use Glide to load the image
         if (!TextUtils.isEmpty(event.getPosterURL())) {
             Glide.with(holder.itemView.getContext())
                     .load(event.getPosterURL())
-                    .placeholder(R.drawable.ic_entrant_profile) // A placeholder drawable
-                    .error(R.drawable.ic_trash_bin) // An error drawable
+                    .placeholder(R.drawable.ic_images) // A generic placeholder
+                    .error(R.drawable.ic_delete)     // An error drawable
                     .into(holder.imagePreview);
         } else {
-            holder.imagePreview.setImageResource(R.drawable.ic_images); // Default image if no URL
+            // Should not happen if fragment filters correctly, but good to have a fallback
+            holder.imagePreview.setImageResource(R.drawable.ic_images);
         }
 
-
+        // Set the click listener for the delete button
         holder.deleteButton.setOnClickListener(v -> {
             if (deleteListener != null) {
-                deleteListener.onDelete(event);
+                // Pass both the event and its position for efficient removal
+                deleteListener.onDelete(event, holder.getAdapterPosition());
             }
         });
     }
 
+    /**
+     * Returns the total number of items in the data set held by the adapter.
+     *
+     * @return The total number of events in the {@code eventList}.
+     */
     @Override
     public int getItemCount() {
         return eventList.size();
     }
 
+    /**
+     * Updates the data set with a new list of events and notifies the adapter.
+     *
+     * @param newEventList The new list of events to display.
+     */
     public void updateData(List<Event> newEventList) {
         this.eventList.clear();
         this.eventList.addAll(newEventList);
         notifyDataSetChanged();
     }
 
+    /**
+     * Removes an item from the list at a specific position.
+     * This is more efficient than reloading the entire list from the database.
+     *
+     * @param position The position of the item to remove.
+     */
+    public void removeItem(int position) {
+        if (position >= 0 && position < eventList.size()) {
+            eventList.remove(position);
+            notifyItemRemoved(position);
+        }
+    }
+
+    /**
+     * A {@link RecyclerView.ViewHolder} that describes an image item view.
+     * <p>
+     * It holds the UI components for a single image card, including {@link ImageView}s
+     * for the preview and delete button, and {@link TextView}s for the event's name
+     * and description.
+     */
     public static class ImageViewHolder extends RecyclerView.ViewHolder {
         ImageView imagePreview, deleteButton;
         TextView eventName, eventDescription;
@@ -85,8 +162,9 @@ public class AdminImagesAdapter extends RecyclerView.Adapter<AdminImagesAdapter.
         public ImageViewHolder(@NonNull View itemView) {
             super(itemView);
             imagePreview = itemView.findViewById(R.id.image_preview);
-            eventName = itemView.findViewById(R.id.image_uploader); // Re-using this TextView
-            eventDescription = itemView.findViewById(R.id.image_upload_date); // Re-using this TextView
+            // Re-using the layout's TextViews to show event context
+            eventName = itemView.findViewById(R.id.image_uploader);
+            eventDescription = itemView.findViewById(R.id.image_upload_date);
             deleteButton = itemView.findViewById(R.id.delete_button);
         }
     }
--- code/app/src/main/java/com/example/zypherevent/ui/admin/events/AdminImagesFragment.java
+++ code/app/src/main/java/com/example/zypherevent/ui/admin/events/AdminImagesFragment.java
@@ -1,60 +1,113 @@
 package com.example.zypherevent.ui.admin.events;
 
-
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import com.example.zypherevent.Database;
 import com.example.zypherevent.Event;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
+import com.example.zypherevent.R;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 /**
- * @author Arunavo Dutta
- * @version 1.3
+ * A fragment for administrators to browse and remove event posters.
+ *
+ * <p>
+ * This class provides the user interface and logic for fulfilling the following user stories:
+ * <ul>
+ * <li><b>US 03.06.01:</b> As an administrator, I want to be able to browse images that are
+ * uploaded so I can remove them if necessary.</li>
+ * <li><b>US 03.03.01:</b> As an administrator, I want to be able to remove images.</li>
+ * </ul>
+ * </p>
+ *
+ * <p>
+ * This fragment extends {@link AdminBaseListFragment} and uses {@link AdminImagesAdapter}
+ * to display a list of all events that have a non-null {@code posterURL}.
+ * The "remove" functionality is implemented by setting the event's {@code posterURL} field
+ * to {@code null} in the Firestore database, which effectively removes the image from the app.
+ * </p>
+ *
+ * @author Arunavo Dutta (Refactored)
+ * @version 2.0
  * @see AdminBaseListFragment
- * @see Event
  * @see AdminImagesAdapter
- * @see "res/navigation/admin_navigation.xml"
+ * @see Event
+ * @see Database
  */
-
-public class AdminImagesFragment extends AdminBaseListFragment {
+public class AdminImagesFragment extends AdminBaseListFragment implements AdminImagesAdapter.OnDeleteListener {
 
     private static final String TAG = "AdminImagesFragment";
     private AdminImagesAdapter adapter;
     private Database database;
-    private List<Event> eventList = new ArrayList<>();
+    private List<Event> eventListWithPosters = new ArrayList<>();
+    private Button refreshButton;
 
+    /**
+     * Called when the fragment's view has been created.
+     * <p>
+     * Initializes the database, sets up the RecyclerView with the {@link AdminImagesAdapter},
+     * configures the refresh button, and triggers the initial loading of images.
+     *
+     * @param view The View returned by {@link #onCreateView(android.view.LayoutInflater, android.view.ViewGroup, Bundle)}.
+     * @param savedInstanceState If non-null, this fragment is being re-constructed from a previous saved state.
+     */
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+        super.onViewCreated(view, savedInstanceState); // Sets up recyclerView
 
         database = new Database();
 
-        // Pass the deleteEventPoster method reference to the adapter's listener
-        adapter = new AdminImagesAdapter(eventList, this::deleteEventPoster);
-
+        // Initialize Adapter with the empty list and set this fragment as the delete listener
+        adapter = new AdminImagesAdapter(eventListWithPosters, this);
         recyclerView.setAdapter(adapter);
 
-        fetchEventPosters();
+        // --- REFRESH BUTTON LOGIC ---
+        refreshButton = view.findViewById(R.id.refresh_button);
+        refreshButton.setOnClickListener(v -> {
+            Toast.makeText(getContext(), "Refreshing images...", Toast.LENGTH_SHORT).show();
+            loadImages();
+        });
+        // --- END REFRESH LOGIC ---
+
+        // Load the images for the first time
+        loadImages();
     }
 
-    private void fetchEventPosters() {
+    /**
+     * Fetches all events from the database and filters them to show only those with posters.
+     * This fulfills <b>US 03.06.01 (Browse images)</b>.
+     * <p>
+     * This method adds a safety check to prevent crashes if the database callback
+     * returns after the fragment has been detached from the activity.
+     */
+    private void loadImages() {
+        Log.d(TAG, "Attempting to query 'events' collection to find posters...");
+
         database.getAllEventsList().addOnCompleteListener(task -> {
+            // **CRASH PREVENTION**: Check if the Fragment is still added to its Activity.
+            // If not, (e.g., user navigated away), do not proceed with UI updates.
+            if (!isAdded() || getContext() == null) {
+                Log.w(TAG, "loadImages callback received, but fragment is detached. Aborting UI update.");
+                return;
+            }
+
             if (task.isSuccessful()) {
                 List<Event> allEvents = task.getResult();
                 if (allEvents != null) {
-                    // Filter events that have a posterURL
-                    List<Event> eventsWithPosters = allEvents.stream()
+                    // Filter the list to include only events that have a posterURL
+                    List<Event> filteredList = allEvents.stream()
                             .filter(event -> event.getPosterURL() != null && !event.getPosterURL().isEmpty())
                             .collect(Collectors.toList());
-                    adapter.updateData(eventsWithPosters);
+
+                    Log.d(TAG, "Found " + filteredList.size() + " events with posters.");
+                    adapter.updateData(filteredList);
                 }
             } else {
                 Log.e(TAG, "Error getting documents: ", task.getException());
@@ -64,53 +117,84 @@ public class AdminImagesFragment extends AdminBaseListFragment {
     }
 
     /**
-     * Deletes the poster for a given event from both Firestore and Firebase Storage.
+     * Handles the delete image request from the adapter.
+     * This fulfills <b>US 03.03.01 (Remove images)</b>.
+     * <p>
+     * It shows a confirmation dialog. If confirmed, it proceeds to nullify the
+     * {@code posterURL} in the Firestore document for the specified event.
      *
-     * @param event The event whose poster is to be deleted.
+     * @param event The {@link Event} object whose poster is to be "deleted".
+     * @param position The adapter position of the item, used for efficient UI removal.
      */
-    private void deleteEventPoster(Event event) {
-        String posterUrl = event.getPosterURL();
-        if (posterUrl == null || posterUrl.isEmpty()) {
-            Toast.makeText(getContext(), "No image to delete for this event.", Toast.LENGTH_SHORT).show();
+    @Override
+    public void onDelete(Event event, int position) {
+        // **CRASH PREVENTION**: Check context before showing a dialog.
+        if (getContext() == null) {
+            Log.e(TAG, "Cannot show delete dialog, context is null.");
             return;
         }
 
-        // First, set the posterURL in the Firestore document to null
-        event.setPosterURL(null);
-        database.setEventData(event.getUniqueEventID(), event)
-                .addOnSuccessListener(aVoid -> {
-                    Log.d(TAG, "Event poster URL removed from Firestore for event: " + event.getEventName());
-                    // If Firestore update is successful, delete the image from Firebase Storage
-                    deleteImageFromStorage(posterUrl);
+        // 1. Show Confirmation Dialog
+        new AlertDialog.Builder(getContext())
+                .setTitle("Confirm Removal")
+                .setMessage("Are you sure you want to remove the poster for '" + event.getEventName() + "'? This action cannot be undone.")
+                .setPositiveButton("Remove", (dialog, which) -> {
+                    // User confirmed, proceed with deletion
+                    removeImageFromEvent(event, position);
                 })
-                .addOnFailureListener(e -> {
-                    Log.e(TAG, "Failed to remove poster URL from Firestore", e);
-                    Toast.makeText(getContext(), "Failed to update event details.", Toast.LENGTH_SHORT).show();
-                    // IMPORTANT: Restore the URL locally if the database update fails
-                    // This prevents an inconsistent state where the UI might not match the backend
-                    event.setPosterURL(posterUrl);
-                });
+                .setNegativeButton("Cancel", (dialog, which) -> {
+                    // User cancelled, do nothing
+                    dialog.dismiss();
+                })
+                .show();
     }
 
     /**
-     * Deletes an image file from Firebase Storage using its download URL.
+     * Performs the database operation to "remove" the image.
+     * <p>
+     * This method sets the {@code posterURL} of the given event to {@code null} and then
+     * updates the event in the Firestore database using {@link Database#setEventData(Long, Event)}.
+     * <p>
+     * On success, it removes the item from the adapter locally for an immediate and
+     * efficient UI update, rather than re-fetching the entire list.
      *
-     * @param imageUrl The full HTTPS download URL of the image to be deleted.
+     * @param event The {@link Event} object to update.
+     * @param position The adapter position, to be passed to the adapter for removal.
      */
-    private void deleteImageFromStorage(String imageUrl) {
-        FirebaseStorage storage = FirebaseStorage.getInstance();
-        StorageReference storageRef = storage.getReferenceFromUrl(imageUrl);
+    private void removeImageFromEvent(Event event, int position) {
+        if (event == null || event.getUniqueEventID() == null) {
+            Toast.makeText(getContext(), "Error: Invalid event.", Toast.LENGTH_SHORT).show();
+            return;
+        }
 
-        storageRef.delete()
+        // Set the poster URL to null
+        event.setPosterURL(null);
+
+        // 2. Update the Event in Firestore
+        database.setEventData(event.getUniqueEventID(), event)
                 .addOnSuccessListener(aVoid -> {
-                    Log.d(TAG, "Image successfully deleted from Firebase Storage.");
-                    Toast.makeText(getContext(), "Image deleted successfully.", Toast.LENGTH_SHORT).show();
-                    // Refresh the list from the database to reflect the change in the UI
-                    fetchEventPosters();
+                    // **CRASH PREVENTION**: Check if fragment is still attached
+                    if (!isAdded() || getContext() == null) {
+                        Log.w(TAG, "Image removal successful, but fragment is detached.");
+                        return;
+                    }
+
+                    Log.d(TAG, "Successfully nulled posterURL in Firestore for: " + event.getEventName());
+                    Toast.makeText(getContext(), "Image removed.", Toast.LENGTH_SHORT).show();
+
+                    // 3. Remove the item from the adapter locally
+                    // This is much faster than calling loadImages() again.
+                    adapter.removeItem(position);
                 })
-                .addOnFailureListener(exception -> {
-                    Log.e(TAG, "Failed to delete image from Firebase Storage", exception);
-                    Toast.makeText(getContext(), "Failed to delete image file.", Toast.LENGTH_SHORT).show();
+                .addOnFailureListener(e -> {
+                    // **CRASH PREVENTION**: Check if fragment is still attached
+                    if (!isAdded() || getContext() == null) {
+                        Log.w(TAG, "Image removal failed, and fragment is detached.");
+                        return;
+                    }
+
+                    Log.e(TAG, "Failed to update event to remove posterURL", e);
+                    Toast.makeText(getContext(), "Failed to remove image. Please try again.", Toast.LENGTH_SHORT).show();
                 });
     }
 }

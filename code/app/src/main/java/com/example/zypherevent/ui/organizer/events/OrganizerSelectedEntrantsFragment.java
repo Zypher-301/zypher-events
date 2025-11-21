b11b3a785667c6270b474046b299ee2e0fd09d11
ADS2001 <arunavodutta21@gmail.com>
2025-11-20 22:13:41 -0700

    Update OrganizerSelectedEntrantsFragment.java

--- code/app/src/main/java/com/example/zypherevent/ui/organizer/events/OrganizerSelectedEntrantsFragment.java
+++ code/app/src/main/java/com/example/zypherevent/ui/organizer/events/OrganizerSelectedEntrantsFragment.java
@@ -295,16 +295,47 @@ public class OrganizerSelectedEntrantsFragment extends Fragment {
     private static class EntrantInfoAdapter extends RecyclerView.Adapter<EntrantInfoAdapter.EntrantInfoViewHolder> {
         private List<Entrant> entrants;
 
+        /**
+         * Constructs an adapter with an initial list of entrants.
+         *
+         * @param entrants The list of {@link Entrant} objects to be displayed.
+         *                 This list can be empty if no data is available initially.
+         */
         public EntrantInfoAdapter(List<Entrant> entrants) {
             this.entrants = entrants;
         }
 
+        /**
+         * Updates the list of entrants displayed in the RecyclerView.
+         * This method clears the current list of entrants, adds all entrants from the
+         * new list, and then notifies the adapter that the data set has changed,
+         * which triggers a refresh of the RecyclerView.
+         *
+         * @param newEntrants The new list of {@link Entrant} objects to display.
+         */
         public void updateData(List<Entrant> newEntrants) {
             this.entrants.clear();
             this.entrants.addAll(newEntrants);
             notifyDataSetChanged();
         }
 
+        /**
+         * Called when RecyclerView needs a new {@link EntrantInfoViewHolder} of the given type to represent an item.
+         * <p>
+         * This new ViewHolder should be constructed with a new View that can represent the items
+         * of the given type. You can either create a new View manually or inflate it from an XML
+         * layout file.
+         * <p>
+         * The new ViewHolder will be used to display items of the adapter using
+         * {@link #onBindViewHolder(EntrantInfoViewHolder, int)}. Since it will be re-used to display
+         * different items in the data set, it is a good idea to cache references to sub-views of
+         * the View to avoid unnecessary {@link View#findViewById(int)} calls.
+         *
+         * @param parent The ViewGroup into which the new View will be added after it is bound to
+         *               an adapter position.
+         * @param viewType The view type of the new View.
+         * @return A new EntrantInfoViewHolder that holds a View of the given view type.
+         */
         @NonNull
         @Override
         public EntrantInfoViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -313,6 +344,19 @@ public class OrganizerSelectedEntrantsFragment extends Fragment {
             return new EntrantInfoViewHolder(view);
         }
 
+        /**
+         * Called by RecyclerView to display the data at the specified position. This method
+         * updates the contents of the {@link EntrantInfoViewHolder#itemView} to reflect the
+         * entrant at the given position in the data set.
+         *
+         * <p>It retrieves the {@link Entrant} object from the list, formats a string with the
+         * entrant's full name, and another string with their email and phone number (if available).
+         * These strings are then set as the text for the two {@link TextView}s in the holder's view.</p>
+         *
+         * @param holder   The ViewHolder which should be updated to represent the contents of the
+         *                 item at the given position in the data set.
+         * @param position The position of the item within the adapter's data set.
+         */
         @Override
         public void onBindViewHolder(@NonNull EntrantInfoViewHolder holder, int position) {
             Entrant entrant = entrants.get(position);
@@ -327,6 +371,11 @@ public class OrganizerSelectedEntrantsFragment extends Fragment {
             holder.text2.setText(details);
         }
 
+        /**
+         * Returns the total number of items in the data set held by the adapter.
+         *
+         * @return The total number of entrants in the list.
+         */
         @Override
         public int getItemCount() {
             return entrants.size();

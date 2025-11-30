2c4b22e5c9c8ee0be769e5f85845ce588fe9ca06
ADS2001 <arunavodutta21@gmail.com>
2025-11-30 15:31:47 -0700

    Update EntrantTests.java

--- code/app/src/androidTest/java/com/example/zypherevent/EntrantTests.java
+++ code/app/src/androidTest/java/com/example/zypherevent/EntrantTests.java
@@ -35,7 +35,7 @@ import java.util.stream.Collectors;
 @RunWith(AndroidJUnit4.class)
 public class EntrantTests {
 
-    // Isolated test collections
+    // Test collections
     private static final String TEST_USERS_COLLECTION = "test_users";
     private static final String TEST_EVENTS_COLLECTION = "test_events";
     private static final String TEST_NOTIFICATIONS_COLLECTION = "test_notifications";
